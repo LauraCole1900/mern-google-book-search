af40cc729f67ae0dc51c3559b379f04e223f279a
Laura Cole <lauracole1900@comcast.net>
2021-11-17 09:07:03 -0700

    added userController and userRoutes

---
+++ server/api_routes/userRoutes.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const userController = require("../controllers/userController.js");
+
+// stem "/api/user"
+router.route("/post")
+  .post(userController.create);
+
+
+router.route("/:email")
+  .get(userController.findByEmail);
+
+
+router.route("/update/:id")
+  .put(userController.updateUser);
+
+
+router.route("/delete/:id")
+  .delete(userController.removeUser);
+
+  
+module.exports = router;
---
+++ server/controllers/userController.js
@@ -0,0 +1,43 @@
+const ObjectId = require("mongodb").ObjectId;
+const db = require("../models")
+
+module.exports = {
+  // POST new user to database
+  create: function (req, res) {
+    console.log("from userController create", req.body.email)
+    db.User
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(500).json(err))
+  },
+
+
+  // GET user by email
+  findByEmail: function (req, res) {
+    console.log("from userController findByEmail", req.params.email)
+    db.User
+      .findOne({ email: req.params.email })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err))
+  },
+
+
+  // PUT user
+  updateUser: function (req, res) {
+    console.log("from userController updateUser", req.body)
+    db.User
+      .findOneAndUpdate({ email: req.body.email }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err))
+  },
+
+
+  // DELETE user
+  removeUser: function (req, res) {
+    db.User
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err))
+  }
+}
