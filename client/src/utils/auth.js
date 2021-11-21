6a0978f8dfaa8b921b95d9c851c9c61806094fba
Laura Cole <lauracole1900@comcast.net>
2021-11-21 13:26:29 -0700

    added auth code front and back; installed jwt-decode dependency on front

---
+++ client/src/utils/auth.js
@@ -0,0 +1,47 @@
+import decode from 'jwt-decode';
+
+class AuthService {
+  // GET user data
+  getProfile() {
+    return decode(this.getToken());
+  }
+
+  // Check if user is logged in
+  loggedIn() {
+    // Check if there's a saved token and that it's still valid
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
+  }
+
+  // Check if token is expired
+  isTokenExpired(token) {
+    try {
+      const decoded = decode(token);
+      if (decoded.exp < Date.now() / 1000) {
+        return true;
+      } else return false;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  getToken() {
+    // Retrieves the user token from localStorage
+    return localStorage.getItem('id_token');
+  }
+
+  login(idToken) {
+    // Saves user token to localStorage
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
+  }
+
+  logout() {
+    // Clears user token and profile data from localStorage
+    localStorage.removeItem('id_token');
+    // Reloads the page and resets the state of the application
+    window.location.assign('/');
+  }
+}
+
+export default new AuthService();
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,6 +5,33 @@ const secret = process.env.JWT_SECRET;
 const expiration = "1h";
 
 module.exports = {
+
+  authMiddleware: function (req, res, next) {
+    // Token can be sent via req.query or req.headers
+    let token = req.query.token || req.headers.authorization;
+
+    // ["Bearer", "<tokenvalue>"]
+    if (req.headers.authorization) {
+      token = token.split(" ").pop().trim();
+    }
+
+    if (!token) {
+      return res.status(400).json({ message: "Token not found" });
+    }
+
+    // Verify token and pull user data from it
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log("Invalid token");
+      return res.status(400).json({ message: "Invalid token" });
+    }
+
+    // send to next endpoint
+    next();
+  },
+
   signToken: function ({ email, userName, _id }) {
     const payload = { email, userName, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
