9c2ee62e57142bfe887cf3cc2eadd8235cf06bae
Laura Cole <lauracole1900@comcast.net>
2021-11-23 08:51:27 -0700

    started functionality on login page

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const LoginPage = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: ""
+  })
+
+  // Handles input changes to form fields
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  // Handles click on "submit" button
+  const handleFormSubmit = () => {
+    
+  }
 
   return (
     <>
-    
+
     </>
   )
 }
