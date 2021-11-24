f1c23b931637a6ec81a0aa372c21d70db8bf31bf
Laura Cole <lauracole1900@comcast.net>
2021-11-23 21:34:55 -0700

    added save and delete icons for buttons, restyled save/delete button

--- client/src/components/bookCard/index.js
+++ client/src/components/bookCard/index.js
@@ -1,9 +1,22 @@
 import React from "react";
 import { Button, Card, Col, Image, Row } from "react-bootstrap";
-import Auth from '../../utils/auth';
+import Auth from "../../utils/auth";
+import saveIcon from "../../icons/save-icon.png";
+import deleteIcon from "../../icons/trash-can.png";
 import "./style.css";
 
 const BookCard = ({ thisBook }) => {
+  // Breaks down the URL
+  const urlArray = window.location.href.split("/")
+  const urlWhere = urlArray[urlArray.length - 1]
+
+  const saveBook = () => {
+
+  }
+
+  const deleteBook = () => {
+
+  }
 
   return (
     <>
@@ -19,17 +32,40 @@ const BookCard = ({ thisBook }) => {
                 <p>by {thisBook.volumeInfo.authors.join("; ")}</p>}
               <p>Click <a href={thisBook.volumeInfo.previewLink} target="_blank" rel="noreferrer">here</a> for more information</p>
             </Col>
-            {Auth.loggedIn() &&
-              <Col sm={2}>
+            {/* {Auth.loggedIn() &&
+              urlWhere !== "my_books" &&
+              <Col sm={2} className="right">
                 <Button
                   data-toggle="popover"
                   title="Save Book"
-                  className="button gButton"
+                  className="button iconButton"
                   data-btnname="saveBook"
-                  // onClick={saveBook}
+                  onClick={saveBook}
                   type="button"
-                >Save Book</Button>
-              </Col>}
+                ><Image fluid src={saveIcon} className="icon" alt="Save" /></Button>
+              </Col>} */}
+            {Auth.loggedIn() &&
+              (urlWhere === "my_books"
+                ? <Col sm={2} className="right">
+                  <Button
+                    data-toggle="popover"
+                    title="Delete Book"
+                    className="button iconButton"
+                    data-btnname="deleteBook"
+                    onClick={deleteBook}
+                    type="button"
+                  ><Image fluid src={deleteIcon} className="icon" alt="Save" /></Button>
+                </Col>
+                : <Col sm={2} className="right">
+                  <Button
+                    data-toggle="popover"
+                    title="Save Book"
+                    className="button iconButton"
+                    data-btnname="saveBook"
+                    onClick={saveBook}
+                    type="button"
+                  ><Image fluid src={saveIcon} className="icon" alt="Save" /></Button>
+                </Col>)}
           </Row>
         </Card.Header>
         <Card.Body>
